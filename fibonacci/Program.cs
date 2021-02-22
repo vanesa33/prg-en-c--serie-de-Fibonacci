d20a3b151bf2f754cebc78bd5a490157208313c1
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:34:33 -0300

    Add files via upload

---
+++ fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SERIE_DE_FIBONACCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte CAN, K;
+            int A, B, C;
+            string linea;
+            Console.Write("CUANTOS NÚMEROS: "); linea = Console.ReadLine();
+            CAN = byte.Parse(linea);
+            A = 1; B = 1;
+            Console.Write(A + " " + B + " ");
+            for (K = 3; K <= CAN; K++)
+            {
+                C = A + B;
+                Console.Write(C + " ");
+                //INTERCAMBIO DE VALORES
+                A = B;
+                B = C;
+            }
+            Console.WriteLine();
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
